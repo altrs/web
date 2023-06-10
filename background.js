a464314c8942d80a747959ce357aed5ddbb6c2d3
Robin Altman <robinhaltman@gmail.com>
2023-06-10 14:10:47 -0400

    finally somthing

--- background.js
+++ background.js
@@ -1,66 +1,57 @@
-function setup (){
-	var canvas = createCanvas(windowWidth, windowHeight, WEBGL);
-	canvas.style('display', 'block');
-	angleMode(DEGREES);
+var mySize;
+let rectWidth = 100;
+let rectHeight = 100;
+let reverse = false; // Flag to indicate if the rotation should be reversed
+let startTime; // Variable to store the starting time
+
+function setup() {
+  var canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+  canvas.style('display', 'absolute');
+  canvas.style('z-index', '1');
+  angleMode(DEGREES);
+
+  startTime = millis(); // Store the starting time
 }
 
 function windowResized() {
- 	resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight);
 }
 
-function draw(){
-	background(96, 124, 217);
-	translate(-40, 0);
-	rotateX(60);
-	noFill();
-	blendMode(BLEND);
+function draw() {
+  background(93, 147, 225);
 
-	for(var i = 0; i < 50; i++){
+  push();
+  translate(-40, 0);
+  rotateX(60);
+  noFill();
+  blendMode(BLEND);
 
-		 // var r = map(sin(frameCount/2), -1, 1, 100, 200);
-		 // var g = map(i, 0, 50, 100, 200);
-		 // var b = map(cos(frameCount), -1, 1, 200, 100);
-		 // stroke(r,g,b);
+  if (reverse) {
+    // Reverse the rotation
+    rotate(-frameCount / 100);
+  } else {
+    rotate(frameCount / 100);
+  }
 
-		rotate(frameCount/100);
+  for (var i = 0; i < 50; i++) {
+    beginShape();
+    for (var j = 0; j < 180; j += 20) {
+      var rad = i * 10;
+      var x = rad * cos(j);
+      var y = rad * sin(j);
+      var z = sin(frameCount * 2 + i * 5) * 50;
 
-		beginShape();
-		for(var j = 0; j < 180; j += 30){
-			var rad = i * 10;
-			var x = rad * cos(j*4);
-			var y = rad * sin(j*4);
-			var z = sin(frameCount * 2 + i * 5) * 25;
-			
-			stroke(90, 172, 134);
-			strokeWeight(5);
-			vertex(x, y ,z);
+      stroke(181, 214, 124);
+      strokeWeight(1);
+      vertex(x, y, z);
+      vertex(-x, y, z);
+    }
+    endShape(CLOSE);
+  }
+  pop();
 
-			stroke(132, 160, 36);
-			strokeWeight(5);
-			vertex(x, y ,z);
-		}
-		endShape(CLOSE);
-	}
-
-	blendMode(OVERLAY);
-	rotateX(60);
-	for(var i = 0; i < 50; i++){
-		rotate(frameCount/100);
-
-		beginShape();
-		for(var j = 0; j < 360; j += 20){
-			var rad = i * 10;
-			var x = rad * cos(j) + 100 / i;
-			var y = rad * sin(j) - 100 * sin(i);
-			var z = sin(frameCount * 2 + i * 5) * 50;
-			
-			stroke(30);
-			strokeWeight(1);
-			vertex(x, y ,z);
-		}
-		endShape(CLOSE);
-
-
-	}
+  // Check if 5 seconds have passed
+  if (millis() - startTime >= 5000) {
+    reverse = true; // Set the reverse flag to true
+  }
 }
-
