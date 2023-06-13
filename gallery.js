ef81ef2f8376c698af8aeca7deff594cf4895b5f
Robin Altman <robinhaltman@gmail.com>
2023-06-13 12:57:17 -0400

    images plz plx

--- gallery.js
+++ gallery.js
@@ -4,33 +4,33 @@ const selectedImage = document.getElementById('selectedImage');
 const imageIndexes = [1, 2, 3];
 const selectedIndex = null;
 
-imageIndexes.forEach(i => {
-  //container
+for (let i = 0; i < imageIndexes.length; i++) {
+  const index = imageIndexes[i];
+  
+  // container
   const container = document.createElement('div');
   container.classList.add('imageContainer');
 
-  //image
+  // image
   const image = document.createElement('img');
-  image.src = '/assets/threedee/' + i + '.png';
-
+  image.src = '/assets/threedee/' + index + '.png';
   image.classList.add('galleryImg');
 
-  //title
+  // title
   const title = document.createElement('p');
-  title.textContent = `${i}.3DImEN-IMG`;
+  title.textContent = `${index}.3DImEN-IMG`;
   title.classList.add('text');
 
   container.appendChild(image);
   container.appendChild(title);
 
-
   container.addEventListener('click', () => {
     popup.style.transform = `translateY(0)`;
-    selectedImage.src = '/assets/threedee/' + i + '.png';
+    selectedImage.src = '/assets/threedee/' + index + '.png';
   });
-  
+
   gallery.appendChild(container);
-});
+}
 
 popup.addEventListener('click', () => {
   popup.style.transform = `translateY(-100%)`;
