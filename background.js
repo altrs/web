d0814020fdf67bc4b1e3021f278437387225b80b
Robin Altman <robinhaltman@gmail.com>
2023-06-13 11:02:11 -0400

    index

--- background.js
+++ background.js
@@ -1,8 +1,6 @@
 var divsArray = [];
 var rotateVal = 0;
 
-var vid;
-
 for (var i = 1; i <= 5; i++) {
   for (var j = 1; j <= 5; j++) {
     var divId = 'g' + i + '-' + j;
@@ -14,37 +12,45 @@ for (var i = 1; i <= 5; i++) {
 var notActive = [divsArray[1], divsArray[4], divsArray[6], divsArray[9], divsArray[10], divsArray[11], divsArray[13], divsArray[17], divsArray[18], divsArray[19], divsArray[20], divsArray[23], divsArray[24]];
 var active = [divsArray[0], divsArray[2], divsArray[3], divsArray[5], divsArray[7], divsArray[8], divsArray[12], divsArray[14], divsArray[15], divsArray[16], divsArray[21], divsArray[22]];
 
-//var colors = ['#fc7253', '#fa472f', '#fc6753'];
+var colors2 = ['#51f542', '#7ad672', '#729c6e', '#729c6e'];
 var colors = ['#fcff4f', '#fcff36', '#fdff8f', '#ffce47'];
 
-for(let e=0; e<notActive.length; e++){
-  //notActive[e].style.border = "1px solid red";
-
+for (let e = 0; e < notActive.length; e++) {
   notActive[e].addEventListener("click", function() {
     rotateVal = rotateVal + 90;
-    for(let o=0; o<active.length; o++){
+
+    for (let o = 0; o < active.length; o++) {
       active[o].style.border = "1px solid white";
-      var randomIndex = Math.floor(Math.random() * colors.length);
-      var randomString = colors[randomIndex];
-      active[o].style.backgroundColor = randomString;
-      active[o].style.border = "1px solid" + randomString;
-      notActive[e].style.rotate = rotateVal + "deg";
-    }
+      active[o].style.backgroundColor = 'transparent';
+
+      active[o].style.rotate = Math.floor(Math.random() * 180) + 'deg';
+      setTimeout(function() {
+        active[o].style.rotate = '0deg';
+      }, 200);
+
+      for (let x = 0; x < notActive.length; x++) {
+        var randomIndex = Math.floor(Math.random() * colors.length);
+        var randomString = colors[randomIndex];
+        notActive[x].style.backgroundColor = randomString;
+
+        const pTags = notActive[x].getElementsByTagName('p');
+        for (let j = 0; j < pTags.length; j++) {
+          pTags[j].style.color = 'black';
+        }
+      }
+
+      var randomIndex2 = Math.floor(Math.random() * colors2.length);
+      var randomString2 = colors2[randomIndex];
+      active[o].style.border = "2px solid " + randomString2;
+      notActive[e].style.rotate = rotateVal + "deg";    }
   });
 }
-
 
 function setup() {
   var canvas = createCanvas(windowWidth, windowHeight, WEBGL);
   canvas.style('display', 'absolute');
   canvas.style('z-index', '1');
   angleMode(DEGREES);
-
-  vid = createVideo("assets/texture.mp4");
-  vid.size(width, height);
-  vid.loop();
-  vid.hide();
-  vid.volume(0);
 
   startTime = millis(); // Store the starting time
 }
@@ -54,24 +60,6 @@ function windowResized() {
 }
 
 function draw() {
-  // background(77, 95, 232);
 
-  image(vid, 0, 0);
-
-  // rotateX(60);
-  // noFill();
-  // blendMode(BLEND);
-
-  // for(let i = 0; i < 50; i++){
-  //   push();
-  //   rotateX(-60);
-  //   rotate(frameCount / 2);
-  //   stroke(252, 104, 45);
-  //   strokeWeight(8+i);
-  //   ellipse(0, 0, 100*i, 50*i);
-  //   stroke(114, 173, 106);
-  //   ellipse(1, 1, 100*i, 50*i);
-  //   pop();
-  // }
 
 }
---
+++ gallery.js
@@ -0,0 +1,38 @@
+const gallery = document.getElementById('gallery');
+const popup = document.getElementById('popup');
+const selectedImage = document.getElementById('selectedImage');
+const imageIndexes = [1, 2, 3];
+const selectedIndex = null;
+
+imageIndexes.forEach(i => {
+  //container
+  const container = document.createElement('div');
+  container.classList.add('imageContainer');
+
+  //image
+  const image = document.createElement('img');
+  image.src = `/assets/threedee/${i}.png`;
+  image.classList.add('galleryImg');
+
+  //title
+  const title = document.createElement('p');
+  title.textContent = `${i}.3DImEN-IMG`;
+  title.classList.add('text');
+
+  container.appendChild(image);
+  container.appendChild(title);
+
+
+  container.addEventListener('click', () => {
+    popup.style.transform = `translateY(0)`;
+    selectedImage.src = `/assets/threedee/${i}.png`;
+  });
+
+
+  gallery.appendChild(container);
+});
+
+popup.addEventListener('click', () => {
+  popup.style.transform = `translateY(-100%)`;
+  popup.src = '';
+});
